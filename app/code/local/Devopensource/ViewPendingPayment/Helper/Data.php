6195987dca83c8aafcb51ee81ed828b4dca43b2b
Marc Bernabeu Moreno <marc.bernabeu@devopensource.com>
2015-03-12 18:54:24 +0100

    commit inicial

---
+++ app/code/local/Devopensource/ViewPendingPayment/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @category    Devopensource
+ * @package		Devopensource_ViewPendingPayment
+ * @author      Marc Bernabeu <marc.bernabeu@devopensource.com>
+ * @version     0.1.0
+ * @copyright   Copyright (c) 2015 Devopensource
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Devopensource_ViewPendingPayment_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
